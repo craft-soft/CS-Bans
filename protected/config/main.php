9661d2d30be9c27f9be433998f3f5c81d9a359b2
iamkapman <me@kapman.ru>
2014-01-05 16:32:51 +0400

    Gii

--- protected/config/main.php
+++ protected/config/main.php
@@ -71,15 +71,7 @@ return array(
 	),
 	
 	'modules'=>array_replace($modules, array(
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'1232580',
-			'ipFilters'=>array(),
-			'generatorPaths'=>array(
-				'bootstrap.gii',
-			),
-		),
-		//*/
+		
 	)),
 
 	// Компоненты приложения
