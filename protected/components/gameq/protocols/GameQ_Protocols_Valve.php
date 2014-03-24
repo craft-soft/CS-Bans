e566c59b2b9e646ea635a329a31ffd032ba4af85
Alexander <urichalex@mail.ru>
2014-03-24 15:26:33 +0400

    Добавлена поддержка серверов Half-Life 1

--- protected/components/gameq/protocols/GameQ_Protocols_Valve.php
+++ protected/components/gameq/protocols/GameQ_Protocols_Valve.php
@@ -29,7 +29,7 @@ require_once GAMEQ_BASE . 'Protocol.php';
  * @author          Tom Buskens <t.buskens@deviation.nl>
  * @version         $Revision: 1.1 $
  */
-class GameQ_Protocol_valve extends GameQ_Protocol
+class GameQ_Protocol_Valve extends GameQ_Protocol
 {
     /*
      * Status
